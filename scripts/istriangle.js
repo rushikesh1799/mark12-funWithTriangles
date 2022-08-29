2165061c5a537ee68b198fe6b07f65b75e06d5eb
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2022-08-29 12:40:01 +0530

    Update isTriangle.js
    Added Validation checks code

--- scripts/istriangle.js
+++ scripts/istriangle.js
@@ -4,7 +4,7 @@ const outputEl = document.querySelector("#output");
 
 const calculateBtn = document.querySelector(".check-btn")
 
-// console.log(outputEl);
+
 
 function calculateSumOfAngles(angle1, angle2, angle3) {
   const sumOfAngles = angle1 + angle2 + angle3;
@@ -21,12 +21,20 @@ function isTriangle(e) {
     Number(inputs[1].value),
     Number(inputs[2].value)
   );
-  // console.log(sumOfAngles);
-  if (sumOfAngles === 180) {
+  // console.log(Number(inputs[0].value));
+  // console.log(inputs[1].value);
+  // console.log(inputs[2].value);
+  if (inputs[0].value === "" || inputs[1].value === "" || inputs[2].value === "") {
+    outputEl.innerText = "Please enter all the required three angles of Triangle."
+    // outputEl.innerText = "Angles can not be zero"
+  } else if (inputs[0].value <= 0 || inputs[1].value <= 0 || inputs[2].value <= 0) {
+    outputEl.innerText = "Any angle of Triangle can not be zero, angle value should be greater than or equal to 1."
+    // outputEl.innerText = "Please enter all the three angles"
+  } else if (sumOfAngles === 180) {
     outputEl.innerText = "Yay, the given angles form a triangle.";
   } else {
     outputEl.innerText =
-      "Oh uh, the given angles can not form a triangle. Try again.";
+      "Oh uh, the given angles can not form a triangle. You can Try again.";
   }
   
 }
